666272723c0fa554147d1b9f83ae02b1e370cbe4
Traore Allassane <traoreallassane2255@gmail.com>
2026-06-08 12:51:27 +0000

    fix(Epargne) : correction de bugs

--- app/Repositories/CompteEpargneRepository.php
+++ app/Repositories/CompteEpargneRepository.php
@@ -3,12 +3,15 @@
 namespace App\Repositories;
 
 use App\Models\CompteEpargne;
+use Illuminate\Support\Facades\Auth;
 
 class CompteEpargneRepository
 {
     public function all()
     {
-        return CompteEpargne::withSum("versements as montant_total_compte", "montant_verse")->get();
+        return CompteEpargne::withSum("versements as montant_total_compte", "montant_verse")
+            ->where('user_id', Auth::user()->id)
+            ->get();
     }
 
     public function create(array $data)
