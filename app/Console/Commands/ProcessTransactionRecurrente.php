b103c4d1a833cbc49520c19e9d07205215b3f006
Traore Allassane <traoreallassane2255@gmail.com>
2026-06-24 11:54:07 +0000

    refactor(Transaction Recurrente) : Optimisation de la logique d'excution des transactions recurrentes

--- app/Console/Commands/ProcessTransactionRecurrente.php
+++ app/Console/Commands/ProcessTransactionRecurrente.php
@@ -6,6 +6,7 @@ use App\Models\Transaction;
 use App\Models\TransactionRecurrente;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class ProcessTransactionRecurrente extends Command
 {
@@ -32,41 +33,45 @@ class ProcessTransactionRecurrente extends Command
             ->where("next_run_at", "<=", now())
             ->get();
 
-        // Création d'une transaction pour chaque transaction recurrente trouvée
-        foreach ($transactionRecurrentes as $recurrente) {
-            Transaction::create([
-                "type" => $recurrente->type,
-                "date" => now(),
-                "montant" => $recurrente->montant,
-                "description" => $recurrente->description,
-                "user_id" => $recurrente->user_id,
-                "category_id" => $recurrente->category_id
-            ]);
+        if ($transactionRecurrentes) {
+            // Création d'une transaction pour chaque transaction recurrente trouvée
+            foreach ($transactionRecurrentes as $recurrente) {
+                Transaction::create([
+                    "type" => $recurrente->type,
+                    "date" => now(),
+                    "montant" => $recurrente->montant,
+                    "description" => $recurrente->description,
+                    "user_id" => $recurrente->user_id,
+                    "category_id" => $recurrente->category_id
+                ]);
 
-            // Definition de la prochaine date d'execution
-            $nextDate = null;
-            switch ($recurrente->frequence) {
-                case "Quotidienne":
-                    $nextDate = Carbon::parse($recurrente->next_run_at)->addDay();
-                    break;
+                // Definition de la prochaine date d'execution
+                $nextDate = null;
+                switch ($recurrente->frequence) {
+                    case "Quotidienne":
+                        $nextDate = Carbon::parse($recurrente->next_run_at)->addDay();
+                        break;
 
-                case "Hebdomadaire":
-                    $nextDate = Carbon::parse($recurrente->next_run_at)->addWeek();
-                    break;
+                    case "Hebdomadaire":
+                        $nextDate = Carbon::parse($recurrente->next_run_at)->addWeek();
+                        break;
 
-                case "Mensuelle":
-                    $nextDate = Carbon::parse($recurrente->next_run_at)->addMonth();
-                    break;
+                    case "Mensuelle":
+                        $nextDate = Carbon::parse($recurrente->next_run_at)->addMonth();
+                        break;
 
-                case "Annuelle":
-                    $nextDate = Carbon::parse($recurrente->next_run_at)->addYear();
-                    break;
+                    case "Annuelle":
+                        $nextDate = Carbon::parse($recurrente->next_run_at)->addYear();
+                        break;
+                }
+
+                // Mise à jour de la prochaine date d'execution
+                $recurrente->update(
+                    ["next_run_at" => $nextDate]
+                );
             }
 
-            // Mise à jour de la prochaine date d'execution
-            $recurrente->update(
-                ["next_run_at" => $nextDate]
-            );
+            Log::info("Les transactions récurrentes exécutées avec succès");
         }
     }
 }
